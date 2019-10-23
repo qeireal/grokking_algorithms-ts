ff3ced0e11a20e8f10f5fd27d9307e0226b6e24f
Kirill Galushko <galushko.ka@gmail.com>
2019-10-23 15:08:55 +0300

    update binary search

--- algorithms/01-binary-search/01-binary-search.test.ts
+++ algorithms/01-binary-search/01-binary-search.test.ts
@@ -1,18 +1,90 @@
-import * as fc from 'fast-check';
-import { binarySearch } from './01-binary-search';
+import {
+    anything as fcAnything,
+    Arbitrary,
+    array as fcArray,
+    assert as fcAssert,
+    integer as fcInteger,
+    property as fcProperty,
+    string as fcString
+} from 'fast-check';
 
-test('Property test', () => {
-  fc.assert(
-    fc.property(fc.array(fc.nat()), genArray => {
-      const sortArray = genArray
-        .filter((value, index, arr) => arr.indexOf(value) === index)
-        .sort((a, b) => a - b);
+import {binarySearch} from './01-binary-search';
 
-      sortArray.forEach((element, index) => {
-        const expectedIndex = binarySearch(sortArray, element);
+type SortedValues = number | string | null;
 
-        expect(expectedIndex).toEqual(index);
-      });
-    })
-  );
+const filterFunction = <T>(array: ReadonlyArray<T>, valueToFilter?: T) =>
+    array.filter(
+        (value, index, arr) =>
+        arr.indexOf(value) === index && value !== valueToFilter
+    );
+
+const sortFunction = <T>(array: ReadonlyArray<T>) =>
+    [...array].sort((a: T, b: T) => {
+        if (a > b) {
+            return 1;
+        }
+        if (b > a) {
+            return -1;
+        }
+        return 0;
+    });
+
+const prepareArray = <T>(array: ReadonlyArray<T>, valueToFilter?: T) =>
+    sortFunction(filterFunction(array, valueToFilter));
+
+describe('Property tests', () => {
+  function propertyTest(entity: Arbitrary<SortedValues>) {
+        fcAssert(
+            fcProperty(fcArray(entity), genArray => {
+                const sortedFiltederArray = prepareArray(genArray);
+
+                sortedFiltederArray.forEach((element, index) => {
+                    const expectedIndex = binarySearch(sortedFiltederArray, element);
+
+                    expect(expectedIndex).toEqual(index);
+                });
+            })
+        );
+    }
+    test('with intergers', () => {
+        propertyTest(fcInteger());
+    });
+
+    test('with strings', () => {
+        propertyTest(fcString());
+    });
+
+    test('with value not from the array ', () => {
+        fcAssert(
+            fcProperty(fcArray(fcAnything()), fcAnything(), (array, value) => {
+                const sortedFiltederArray = prepareArray(array, value);
+
+                expect(binarySearch(sortedFiltederArray, value)).toBeNull();
+            })
+        );
+    });
+
+    describe('with nullish values ', () => {
+        test('as array and target', () => {
+            expect(binarySearch(null, null)).toBeNull();
+        });
+
+        test('as array and random target', () => {
+            fcAssert(
+                fcProperty(fcAnything(), value => {
+                    expect(binarySearch(null, value)).toBeNull();
+                })
+            );
+        });
+
+        test('as target and random array ', () => {
+            fcAssert(
+                fcProperty(fcArray(fcAnything()), array => {
+                    const sortedFiltederArray = prepareArray(array, null);
+
+                    expect(binarySearch(sortedFiltederArray, null)).toBeNull();
+                })
+            );
+        });
+    });
 });
--- algorithms/01-binary-search/01-binary-search.ts
+++ algorithms/01-binary-search/01-binary-search.ts
@@ -1,12 +1,18 @@
-export function binarySearch(list: ReadonlyArray<number>, item: number): number | null {
-    let [minIndex, maxIndex] = [0,  list.length - 1];
+export function binarySearch<T>(list: ReadonlyArray<T> | null, item: T): number | null {
+    if (!list) {
+        return null;
+    }
+
+    let [minIndex, maxIndex] = [0, list.length - 1];
 
     while (minIndex <= maxIndex) {
         let midIndex = Math.floor((minIndex + maxIndex) / 2);
         
         if (item === list[midIndex]) {
             return midIndex;
-        } else if (item > list[midIndex]) {
+        }
+
+        if (item > list[midIndex]) {
             minIndex = midIndex + 1;
         } else {
             maxIndex = midIndex - 1;
